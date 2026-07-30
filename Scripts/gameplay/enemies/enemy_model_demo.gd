a58535fdad0f9342d5e507f99694fd186a79e50d
Chris DeLeon <chris@gamkedo.com>
2026-07-29 19:23:59 -0500

    Second stage started with example 3d enemy (script's pretty redundant, anticipating they'll rapidly become less similar)

---
+++ Scripts/gameplay/enemies/enemy_model_demo.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+var speed := 2.0
+var drift_time := 1.5
+
+var direction := Vector3.ZERO
+var time_left := 0.0
+
+var rotation_speed := 5
+
+func _ready():
+	pick_new_direction()
+
+func _process(delta):
+	time_left -= delta
+	if time_left <= 0:
+		pick_new_direction()
+	global_position += direction * speed * delta
+	rotation.y = lerp_angle(rotation.y, atan2(-direction.x, -direction.z), delta * rotation_speed)
+
+func pick_new_direction():
+	var angle = randf_range(0.0, 2.0*PI)
+	var dir := Vector2.from_angle(angle)
+	direction = Vector3(dir.x, 0, dir.y)
+	time_left = drift_time
--- Scripts/menus/level_select.gd
+++ Scripts/menus/level_select.gd
@@ -1,9 +1,12 @@
 extends Node
 
-@onready var area_a: Button = $AreaA
+@export var area_a: Button
 
 func _ready() -> void:
 	area_a.grab_focus()
 
 func _on_area_a_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes - Levels/areaA.tscn")
+
+func _on_area_b_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes - Levels/areaB.tscn")
