f99d818bbcb1065fd8eb1aa06efd234db2e09eea
Chris DeLeon <chris@gamkedo.com>
2026-07-30 15:16:39 -0500

    Billboard example enemy facing to/away graphic switch

--- Scripts/gameplay/enemies/enemy_billboard_demo.gd
+++ Scripts/gameplay/enemies/enemy_billboard_demo.gd
@@ -6,6 +6,9 @@ var drift_time := 1.5
 var direction := Vector3.ZERO
 var time_left := 0.0
 
+@onready var graphic_toward := $BillboardFront
+@onready var graphic_away := $BillboardBack
+
 func _ready():
 	pick_new_direction()
 
@@ -19,4 +22,9 @@ func pick_new_direction():
 	var angle = randf_range(0.0, 2.0*PI)
 	var dir := Vector2.from_angle(angle)
 	direction = Vector3(dir.x, 0, dir.y)
+	
+	# toggle which graphic to show, moving towards or away from camera
+	graphic_toward.visible = dir.y>0
+	graphic_away.visible = !graphic_toward.visible
+	
 	time_left = drift_time
