b5c995767eeebb44d5395c2c09c013787ff797a0
Raymond Zegles <rzwork17@gmail.com>
2026-09-02 18:29:47 -0400

    Added credits button that opens a placeholder credits panel and added UI containers to main menu to center elements

--- Scripts/menus/level_select.gd
+++ Scripts/menus/level_select.gd
@@ -3,7 +3,7 @@ extends Node
 @export var starting_focus: Control
 
 func _ready() -> void:
-	starting_focus.grab_focus()
+	starting_focus.grab_focus.call_deferred()
 	AudioStreamManager.play_bgm("res://Music/terminus1_vorbis.ogg")
 
 func _on_area_a_pressed() -> void:
@@ -14,3 +14,9 @@ func _on_area_b_pressed() -> void:
 
 func _on_area_c_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes - Levels/areaImmersive.tscn")
+
+func _on_credits_button_pressed() -> void:
+	get_node("Credits Panel").show()
+	
+func _on_close_credits_pressed() -> void:
+	get_node("Credits Panel").hide()
