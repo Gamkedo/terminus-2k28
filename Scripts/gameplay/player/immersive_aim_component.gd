13edae8ff11635818690b778e332e7e4f61fa7b5
Scott Wolcott <scottwolcott@gmail.com>
2026-09-01 22:12:03 -0500

    fix mouse aiming in immersive mode

--- Scripts/gameplay/player/immersive_aim_component.gd
+++ Scripts/gameplay/player/immersive_aim_component.gd
@@ -16,6 +16,16 @@ var real_sensitivity: float:
 @export var invert_h: bool = false
 @export var invert_v: bool = false
 
+# Mouse Settings
+var mouse_input: Vector2
+@export var mouse_sensitivity: float = 3
+## this is to make the other sensitivity values more human readable. They are divided by this
+@export var mouse_sense_modifier: float = 1000.0
+var mouse_real_sensitivity: float:
+	get:
+		return mouse_sensitivity / mouse_sense_modifier
+@export var mouse_active: bool = true
+
 
 func handle_aiming(player: Player, delta: float) -> void:
 	if not active:
@@ -31,3 +41,21 @@ func handle_aiming(player: Player, delta: float) -> void:
 	if invert_v:
 		v_amount *= -1
 	player.turret.rotate_x(v_amount * real_sensitivity * v_sense)
+	
+	# mouse part
+	player.turret_pivot.rotate_y(-mouse_input.x * mouse_real_sensitivity)
+	player.turret.rotate_x(-mouse_input.y * mouse_real_sensitivity)
+	mouse_input = Vector2.ZERO
+
+
+
+
+func _input(event: InputEvent) -> void:
+	if not mouse_active:
+		return
+	# this is needed for mouse capture in web games
+	if event is InputEventMouseButton:
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		mouse_input += event.relative
