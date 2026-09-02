b8d3a68deb489d8120a23f531999306f770f3ee2
Christer Kaitila <breakdance@mcfunkypants.com>
2026-09-02 15:57:50 -0700

    in game ambient audio loop (wip)

--- Scripts/menus/level_select.gd
+++ Scripts/menus/level_select.gd
@@ -7,12 +7,15 @@ func _ready() -> void:
 	AudioStreamManager.play_bgm("res://Music/terminus1_vorbis.ogg")
 
 func _on_area_a_pressed() -> void:
+	AudioStreamManager.play_bgm("res://Music/ambience_a.ogg")
 	get_tree().change_scene_to_file("res://Scenes - Levels/areaArcade.tscn")
 
 func _on_area_b_pressed() -> void:
+	AudioStreamManager.play_bgm("res://Music/ambience_a.ogg")
 	get_tree().change_scene_to_file("res://Scenes - Levels/areaSurvivor.tscn")
 
 func _on_area_c_pressed() -> void:
+	AudioStreamManager.play_bgm("res://Music/ambience_a.ogg")
 	get_tree().change_scene_to_file("res://Scenes - Levels/areaImmersive.tscn")
 
 func _on_credits_button_pressed() -> void:
