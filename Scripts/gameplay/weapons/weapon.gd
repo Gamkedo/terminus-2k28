abd7a8d61bdd15df630e857be456205cb189044c
Christer Kaitila <breakdance@mcfunkypants.com>
2026-09-11 13:51:10 -0700

    muzzle flashes

--- Scripts/gameplay/weapons/weapon.gd
+++ Scripts/gameplay/weapons/weapon.gd
@@ -3,6 +3,7 @@ class_name Weapon
 
 @export var sound_effect_path: String
 @export var projectile_scene: PackedScene
+@export var muzzle_flash_scene: PackedScene
 @export_range(0.01, 2.0, 0.01) var fire_rate := 0.2
 
 var reload_time: float = 0.0
@@ -20,6 +21,11 @@ func fire(pos: Vector3, rot: Vector3) -> void:
 	projectile.global_position = pos
 	projectile.global_rotation = rot
 	AudioStreamManager.play_sfx("res://Sound Effects/Lasers/laser_7.wav", AudioStreamManager.PlaybackMode.RANDOM_PITCH)
+	if (muzzle_flash_scene != null):
+		var muzzleflash: Node3D = muzzle_flash_scene.instantiate()
+		get_tree().current_scene.add_child(muzzleflash)
+		muzzleflash.global_position = pos
+		muzzleflash.global_rotation = rot
 
 func can_fire() -> bool:
 	return reload_time <= 0.0
