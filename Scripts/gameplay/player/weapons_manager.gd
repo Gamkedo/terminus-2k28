7e335611b3e5c99cc59214fb69d16f899f9a5c91
Mark Guinn <mark.guinn@finvari.com>
2026-09-12 10:49:58 -0700

    WeaponsManager uses _input instead of _process for handling inputs

--- Scripts/gameplay/player/weapons_manager.gd
+++ Scripts/gameplay/player/weapons_manager.gd
@@ -11,22 +11,22 @@ func _ready() -> void:
 	for child in get_parent().get_children():
 		if child is Weapon: weapons.append(child)
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("debug_cycle_weapon"): # currently "Q"
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_cycle_weapon"): # currently "Q"
 		cycle_weapon()
-	if Input.is_action_just_pressed("debug_previous_weapon"): # currently "E"
+	if event.is_action_pressed("debug_previous_weapon"): # currently "E"
 		previous_weapon()
 	# keyboard number keys to switch weapons
-	if Input.is_action_just_pressed("weapon1"): select_weapon_num(0)
-	if Input.is_action_just_pressed("weapon2"): select_weapon_num(1)
-	if Input.is_action_just_pressed("weapon3"): select_weapon_num(2)
-	if Input.is_action_just_pressed("weapon4"): select_weapon_num(3)
-	if Input.is_action_just_pressed("weapon5"): select_weapon_num(4)
-	if Input.is_action_just_pressed("weapon6"): select_weapon_num(5)
-	if Input.is_action_just_pressed("weapon7"): select_weapon_num(6)
-	if Input.is_action_just_pressed("weapon8"): select_weapon_num(7)
-	if Input.is_action_just_pressed("weapon9"): select_weapon_num(8)
-	if Input.is_action_just_pressed("weapon0"): select_weapon_num(9)
+	if event.is_action_pressed("weapon1"): select_weapon_num(0)
+	if event.is_action_pressed("weapon2"): select_weapon_num(1)
+	if event.is_action_pressed("weapon3"): select_weapon_num(2)
+	if event.is_action_pressed("weapon4"): select_weapon_num(3)
+	if event.is_action_pressed("weapon5"): select_weapon_num(4)
+	if event.is_action_pressed("weapon6"): select_weapon_num(5)
+	if event.is_action_pressed("weapon7"): select_weapon_num(6)
+	if event.is_action_pressed("weapon8"): select_weapon_num(7)
+	if event.is_action_pressed("weapon9"): select_weapon_num(8)
+	if event.is_action_pressed("weapon0"): select_weapon_num(9)
 
 func can_fire() -> bool:
 	return weapons[active_weapon].can_fire()
