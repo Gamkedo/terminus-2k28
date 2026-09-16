c56dac10ffd5da7677d5edc3437e9ae77a41f606
Aescosaurus (Michael B) <michaelbuslik@hotmail.com>
2026-09-16 10:12:27 -0600

    Explosive barrels

--- Scripts/gameplay/world/destructible_scenery.gd
+++ Scripts/gameplay/world/destructible_scenery.gd
@@ -4,16 +4,24 @@ extends Enemy
 ## Particles to spawn when destroyed
 @export var destroy_particles: Array[PackedScene]
 
+@export var anim_player: AnimationPlayer = null
 @export var dest_anim_name: String = "destroy_anim"
 
+@export var destroy_self: bool = false
+
+@export var part_spawn_spot: Node3D = null
+
 @onready var collider = $Area3D
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 func die() -> void:
 	if destroy_particles:
 		for part in destroy_particles:
 			var dest_parts = part.instantiate()
 			get_tree().current_scene.add_child(dest_parts)
-			dest_parts.global_position = global_position
-	collider.queue_free()
-	anim_player.play(dest_anim_name)
+			if part_spawn_spot: dest_parts.global_position = part_spawn_spot.global_position
+			else: dest_parts.global_position = global_position
+	
+	if anim_player: anim_player.play(dest_anim_name)
+	
+	if destroy_self: queue_free()
+	else: collider.queue_free()
