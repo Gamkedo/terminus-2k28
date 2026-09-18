30d2ca97eb60454c89c383ccd58ef96eb3cce629
Chris DeLeon <chris@gamkedo.com>
2026-09-18 17:54:16 -0500

    Pickup animation triggers, doesn't yet evac/stop player

--- Scripts/gameplay/world/survivor_countdown.gd
+++ Scripts/gameplay/world/survivor_countdown.gd
@@ -3,14 +3,22 @@ extends Node
 # based on method covered at:
 # https://www.youtube.com/watch?v=ejRXpRlFa_Y
 
+@export var end_anim: AnimationPlayer
 @onready var label = $TimerLabel
 @onready var timer = $TimerKeeper
+
+var finished := false
 
 func _ready():
 	timer.start()
 
 func _process(delta):
+	if finished:
+		return
+		
 	if timer.time_left <= 0.0:
 		label.text = "RESCUE COMING!"
+		end_anim.play("rescue")
+		finished = true
 	else:
 		label.text = "SURVIVE: %02d" % int(timer.time_left)
