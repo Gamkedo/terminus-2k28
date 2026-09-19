bda3495531eb6922d4191cba491d103e42213106
Tanner G <tannerdev@proton.me>
2026-09-19 13:33:52 -0700

    Added explosion sound to truck destruction

--- Scripts/gameplay/world/truck_scenery.gd
+++ Scripts/gameplay/world/truck_scenery.gd
@@ -3,4 +3,5 @@ extends DestructScenery
 
 func die() -> void:
 	super.die()
+	AudioStreamManager.play_sfx("res://Sound Effects/Explosions/explosion_5.wav", AudioStreamManager.PlaybackMode.RANDOM_PITCH)
 	remove_from_group("enemy")
