fa81abb71d0df6a945b946963122f573f2305d75
Shane B. <shane.bell.dev@gmail.com>
2026-09-19 11:16:15 -0400

    Fixed mouse mode issues with pause menu, main menu buttons not working on return to main menu.

--- Scripts/menus/pause_menu.gd
+++ Scripts/menus/pause_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var prev_mouse_mode: Input.MouseMode
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		pause_or_unpause()
@@ -8,12 +10,15 @@ func _process(_delta: float) -> void:
 func pause_or_unpause():
 	if get_tree().paused == true:
 		$".".hide()
+		Input.mouse_mode = prev_mouse_mode
 		get_tree().paused = false
 		
 	elif get_tree().paused == false:
+		prev_mouse_mode = Input.mouse_mode
+		Input.mouse_mode = Input.MouseMode.MOUSE_MODE_VISIBLE
 		$".".show()
 		get_tree().paused = true
 
 
 func _on_level_select_button_pressed():
-	get_tree().change_scene_to_file("res://level_menu.tscn")
+	SceneChanger.return_to_menu()
