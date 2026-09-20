beffdf16f9b9bb3e6c735939e5696631bc89c4a8
Tanner G <tannerdev@proton.me>
2026-09-20 13:52:38 -0700

    Barrel explosion sound implemented

--- Scripts/gameplay/world/destructible_scenery.gd
+++ Scripts/gameplay/world/destructible_scenery.gd
@@ -18,6 +18,7 @@ func die() -> void:
 	if destroy_particles:
 		for part in destroy_particles:
 			var dest_parts = part.instantiate()
+			AudioStreamManager.play_sfx("res://Sound Effects/Explosions/explosion_car.wav", AudioStreamManager.PlaybackMode.RANDOM_PITCH)
 			get_tree().current_scene.add_child(dest_parts)
 			if part_spawn_spot: dest_parts.global_position = part_spawn_spot.global_position
 			else: dest_parts.global_position = global_position
