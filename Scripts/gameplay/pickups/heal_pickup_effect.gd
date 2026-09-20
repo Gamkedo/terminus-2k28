3e189603ceade029901f52438e98545ba8ea3785
Tanner G <tannerdev@proton.me>
2026-09-20 13:21:32 -0700

    Implemented heal sound + changed pickup playback to standard

--- Scripts/gameplay/pickups/heal_pickup_effect.gd
+++ Scripts/gameplay/pickups/heal_pickup_effect.gd
@@ -4,5 +4,6 @@ extends PickupEffect
 @export var amount: float = 10.0
 
 func apply(player: Player) -> bool:
+	AudioStreamManager.play_sfx("res://Sound Effects/Pickups/pickup_sound_1.wav", AudioStreamManager.PlaybackMode.STANDARD)
 	player.gain_health(amount)
 	return true # always ingesting up powerup, if player didn't need it they should avoid it until ready
--- Scripts/gameplay/pickups/shield_orbit_pickup_effect.gd
+++ Scripts/gameplay/pickups/shield_orbit_pickup_effect.gd
@@ -8,7 +8,7 @@ extends PickupEffect
 
 func apply(player: Player) -> bool:
 	# spawn orbiters around player. If no scene assigned, return false and fail
-	AudioStreamManager.play_sfx("res://Sound Effects/Pickups/pickup_sound_3.wav", AudioStreamManager.PlaybackMode.RANDOM_PITCH)
+	AudioStreamManager.play_sfx("res://Sound Effects/Pickups/pickup_sound_3.wav", AudioStreamManager.PlaybackMode.STANDARD)
 	if shield_orbiter_scene:
 		var orbit_spacing = (PI * 2.0) / orbiter_count
 		for i in orbiter_count:
