a3c3862abdb4af294b967899ad9f524d018c5f7b
Chris DeLeon <chris@gamkedo.com>
2026-09-19 22:06:26 -0500

    Trying to better precache some things still causing hitch

--- Scripts/audio_stream_manager.gd
+++ Scripts/audio_stream_manager.gd
@@ -48,6 +48,7 @@ var sound_preload_attempt: Array[AudioStream] = [
 	preload("uid://dloj8eqy78k88"), # res://Sound Effects/Electricity/electricity_one_shot_v2.wav
 	preload("uid://bkgs7qplm88a2"), # UI/UI_rumble.wav
 	preload("uid://b6de3bc3fgdnh"), # Explosions/explosion_4.wav
+	preload("uid://c0ehc4qrivpgx"), # survivors pickup sound
 ]
 
 var selected_background_music: AudioStream = bg_music_tracks[0]
