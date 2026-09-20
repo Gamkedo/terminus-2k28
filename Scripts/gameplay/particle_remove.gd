6a07c27af3fe0199cfb1fdb55bbdc17527e5eb9e
Chris DeLeon <chris@gamkedo.com>
2026-09-20 13:07:50 -0500

    Adding comment note for hacky way stacked particle removal is being handled

--- Scripts/gameplay/particle_remove.gd
+++ Scripts/gameplay/particle_remove.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+# note: if there are other children (sounds, animations,
+# other particles) make sure CPUParticles3D's time is set
+# so it finishes last, or it'll abruptly cut off siblings
 @onready var child_particle: CPUParticles3D = $CPUParticles3D
 
 func _ready() -> void:
