2db036ba9dd238c3c90a429442c0c5635fbf24ad
Tanner G <tannerdev@proton.me>
2026-09-20 13:25:30 -0700

    Implemented weapon pickup sound

--- Scripts/gameplay/pickups/weapon_pickup_effect.gd
+++ Scripts/gameplay/pickups/weapon_pickup_effect.gd
@@ -7,6 +7,7 @@ extends PickupEffect
 #@export var expires_seconds := 0
 
 func apply(player: Player) -> bool:
+	AudioStreamManager.play_sfx("res://Sound Effects/Pickups/pickup_physical_3.wav", AudioStreamManager.PlaybackMode.STANDARD)
 	if player and player.weapons_manager:
 		return player.weapons_manager.add_weapon(weapon_number)
 	return false
